194f94ebc2b74a5a48bbfb6cb86a29b04c45758d
zhangjie <274588107@qq.com>
2020-05-14 10:50:55 +0800

    addgzip

---
+++ source/longList/LongList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="list-container">
+        <div v-for="(i,index) in list" :key="index">
+            <p>{{i.title}}</p>
+            <p>{{i.content}}</p>
+        </div>
+
+    </div>
+</template>
+<script>
+  export default {
+    name: 'LongList',
+    props: {
+      list: {
+        type: Array,
+        default: () => []
+      }
+    }
+  };
+</script>
+<style>
+    .list-container{
+        height: 100%;
+    }
+</style>
