b264a47362cc48c32504c171589ea609c98fdf2b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-14 02:53:08 +0000

    Deploy zaberz/blog to github.com/zaberz/blog.git:gh-pages

---
+++ longList/index.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <long-list :list="list"></long-list>
+</template>
+<script>
+  import LongList from "./LongList";
+
+  let list = [];
+  let i = 0;
+  while (i < 100) {
+    list.push({
+      title: i,
+      content: `${i}.内容`
+    });
+    i++;
+  }
+  export default {
+
+    components: {
+      LongList
+    },
+    data() {
+      return {
+        list
+      };
+    }
+
+  };
+</script>
